e268c38ae5a14ffc370b9982be98a6f6257f0681
burak <Reayhs1@gmail.com>
2023-03-24 23:50:45 +0300

    Add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  app: {
+    head: {
+      title: "Nuxt Movie",
+      meta: [{ name: "description", content: "Nuxt Movie App" }],
+    },
+  },
 });
